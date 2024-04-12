0bac978b04706cc66556f004e828269f0e2373c7
pybyongbo <pybyongbo@users.noreply.github.com>
2024-04-12 14:05:02 +0000

    deploy: b5ebb81ea0889e2a105cb18c3898077cf5352979

--- src/libs/element-ui/index.js
+++
@@ -1,8 +0,0 @@
-import ElInput from './ElInput.vue';
-import ElRadio from './ElRadio.vue';
-import ElSelect from './ElSelect.vue';
-import ElCheckbox from './ElCheckbox.vue';
-import ElTextarea from './ElTextarea.vue';
-import ElButton from './ElButton.vue';
-
-export { ElInput, ElRadio, ElSelect, ElCheckbox, ElTextarea, ElButton };
